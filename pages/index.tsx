bac6476ca8a90bbc1d3dc29e7fc5853e01608394
flowfordan <flowfordan@gmail.com>
2022-03-30 20:22:57 +0300

    main menu ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import { useEffect, useState } from 'react';
 import { Button, Htag, Rating, Tag, Paragraph } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 
 
   const [rating, setRating] = useState<number>(5);
@@ -32,8 +35,31 @@ function Home(): JSX.Element {
       <Tag size='m' color='red'>Zdarova</Tag>
       <Tag size='m' color='green'>Zdarova</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
+      <ul>
+       {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
+      
     </>
   );
 }
 
 export default withLayout(Home);
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[],
+  firstCategory: number;
+}
