8edec74a804fcad408f3f1a92792b5c96325d971
flowfordan <flowfordan@gmail.com>
2022-03-30 13:44:46 +0300

    layout style adjusments

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -9,17 +9,17 @@ import { FunctionComponent } from "react";
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 
     return (
-        <>
-        <Header />
-        <div>
-            <Sidebar />
-            <div>
+        <div className={styles.wrapper}>
+        <Header className={styles.header}/>
+        
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
                 {children}
             </div>
-        </div>
-        <Footer />
         
-        </>
+        <Footer className={styles.footer}/>
+        
+        </div>
     );
     
 };
