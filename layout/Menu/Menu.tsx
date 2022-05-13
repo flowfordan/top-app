a3314836c1bc2f4091bcb1612e962aa242f5891f
flowfordan <flowfordan@gmail.com>
2022-05-13 11:45:59 +0300

    pages routing setup

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -4,21 +4,12 @@ import { format } from 'date-fns';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { firstLevelMenu } from '../../helpers/helpers';
 
 
-const firstLevelMenu: FirstLevelMenuItem[] = [
-    {route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-    {route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-    {route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-    {route: 'products', name: 'Товары', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+
 
 export const Menu = (): JSX.Element => {
 
