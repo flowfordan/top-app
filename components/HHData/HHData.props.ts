d6d4a8a767a67c8d8672b7e085b18a6e86f01717
flowfordan <flowfordan@gmail.com>
2022-05-17 13:54:21 +0300

    hh component

---
+++ components/HHData/HHData.props.ts
@@ -0,0 +1,6 @@
+import { HTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
+import { HhData } from '../../interfaces/page.interface';
+
+export interface HHDataProps extends HhData {
+    
+}
